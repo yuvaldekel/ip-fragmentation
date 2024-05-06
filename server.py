ba3bca599320608b61e6022641616a09aa56c5e8
yuvallinux <yuvddd05@gmail.com>
2024-05-06 12:45:26 +0300

    create server

---
+++ server.py
@@ -0,0 +1,25 @@
+from scapy.all import IP, UDP, Raw, send
+from socket import socket
+from fragmentation import fragment_string
+
+PORT = 55555
+
+def main():
+    with socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
+        server_socket.bind('0.0.0.0', PORT)
+        
+        (client_data, client_address) = server_socket.recvfrom(1024)
+        client_data = client_data.decode()
+        data_fragments = fragment_string(client_data)
+
+        client_ip, client_port = client_address
+
+        for index, data_fragments in enumerate(data_fragments):
+            i_packet = IP(dst = client_ip, frag = index, id = 1, proto = 'udp')/UDP(dport = client_port, sport = PORT)/Raw(data_fragments[index])
+
+            if index + 1 != len(data_fragments):
+                i_packet[IP].flags = "MF"
+            send(i_packet, iface = 'wlp4s0')    
+
+if __name__ == "__main__":
+    main()
